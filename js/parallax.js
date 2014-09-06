7a941881d42acdd33281743da2f6476ba6fd6b76
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2014-09-06 14:30:15 +0200

    first try

--- js/parallax.js
+++ js/parallax.js
@@ -13,10 +13,19 @@ $(window).ready(function()
 	function refresh() 
 	{
 		var sTop = $(window).scrollTop();
+		var wHeight = $(window).height();
+		
 		$.each($(".plx-img"), function() 
 		{
 			$(this).css("background-position-y", (
-				sTop - $(this).position().top) * (parseFloat($(this).attr('plx-speed')) || 0.1) + (parseFloat($(this).attr('plx-offset')) || 0));
+				sTop - $(this).position().top) * (parseFloat($(this).attr('plx-speed')) || 0.1) + (parseFloat($(this).attr('plx-offset')) || 0) + "px");
+		});
+
+		$.each($(".plx-float"), function(){
+			var ref = $($(this).attr('plx-ref'));
+			console.log(ref);
+			console.log((wHeight/2 + Math.pow( (sTop + wHeight/2 - ref.position().top - ref.height()/2)/(wHeight/2) , 2 ) * (wHeight/2) ));
+			$(this).css("top", (wHeight/2 + Math.pow( (sTop + wHeight/2 - ref.position().top - ref.height()/2)/(wHeight/2) , 2 ) * (wHeight/2) ) + "px");
 		});
 	}
 });
